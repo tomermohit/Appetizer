f1cba4165a09841ff46b94c3765c21de5a99f649
Mohit Tomer <mstomer31@gmail.com>
2023-12-20 23:27:52 +0530

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Mohit Tomer on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
