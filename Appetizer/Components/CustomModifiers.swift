48a4b90f8e74c3e759ef6572c167755e0a454b40
Mohit Tomer <mstomer31@gmail.com>
2023-12-26 20:27:07 +0530

    missing commits

--- Appetizer/Components/CustomModifiers.swift
+++ Appetizer/Components/CustomModifiers.swift
@@ -5,4 +5,20 @@
 //  Created by Mohit Tomer on 26/12/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct StandardButtonStyle: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .buttonStyle(.bordered)
+            .tint(.brandPrimary)
+            .controlSize(.large)
+    }
+}
+
+extension View {
+    func standardButtonStyle() -> some View {
+        self.modifier(StandardButtonStyle())
+    }
+}
--- Appetizer/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizer/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -43,8 +43,14 @@ struct AppetizerDetailView: View {
                 order.add(appetizer)
                 isShowingDetail = false
             }label: {
-               APButton(title: "$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                // APButton(title: "$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
             }
+            .modifier(StandardButtonStyle())
+//            .standardButtonStyle()
+//            .buttonStyle(.bordered)
+//            .tint(.brandPrimary)
+//            .controlSize(.large)
             .padding(.bottom, 30)
         }
         .frame(width: 300, height: 525)
