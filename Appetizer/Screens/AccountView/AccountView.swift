1f2853f9b821d75d4fbd8ee74aedd0232dc54973
Mohit Tomer <mstomer31@gmail.com>
2023-12-26 21:08:15 +0530

    Add Focused on keyboard

--- Appetizer/Screens/AccountView/AccountView.swift
+++ Appetizer/Screens/AccountView/AccountView.swift
@@ -7,17 +7,33 @@
 
 import SwiftUI
 
+enum FormTextField {
+    case firstName, lastName, email
+}
+
 struct AccountView: View {
     
     @StateObject private var viewModel = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField?
     
     var body: some View {
         NavigationStack {
             Form {
                 Section("PERSONAL INFO") {
                     TextField("First Name", text: $viewModel.user.firstName)
+                        .focused($focusedTextField, equals: .firstName)
+                        .onSubmit { focusedTextField = .lastName }
+                        .submitLabel(.next)
+                    
                     TextField("Last Name", text: $viewModel.user.lastName)
+                        .focused($focusedTextField, equals: .lastName)
+                        .onSubmit { focusedTextField = .email }
+                        .submitLabel(.next)
+                    
                     TextField("Email", text: $viewModel.user.email)
+                        .focused($focusedTextField, equals: .email)
+                        .onSubmit { focusedTextField = nil }
+                        .submitLabel(.continue)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .autocorrectionDisabled()
@@ -35,6 +51,11 @@ struct AccountView: View {
                 //.toggleStyle(SwitchToggleStyle.init(tint: .brandPrimary))
             }
                 .navigationTitle("℀ Account")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Button("Dismiss") { focusedTextField = nil }
+                    }
+                }
         }
         .onAppear{
             viewModel.retrieveUser()
