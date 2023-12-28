4a7e507274f71de74760eb066d2e8e8a5b5e2a0b
Mohit Tomer <mstomer31@gmail.com>
2023-12-28 10:22:36 +0530

    Lottie Animation Integrated.

---
+++ Appetizer/Components/Animations/LottieAnimation.swift
@@ -0,0 +1,35 @@
+//
+//  LottieAnimation.swift
+//  Appetizer
+//
+//  Created by ꧁☆༒☬MoʜɩtToɱɘʀ☬༒☆꧂ on 27/12/23.
+//
+
+import SwiftUI
+import Lottie
+
+struct FCLottieView: UIViewRepresentable {
+    var nameLottieJsonFile: String
+    var loopMode: LottieLoopMode = .playOnce
+    var animationView = LottieAnimationView()
+    var completion: (() -> Void)?
+
+    // MARK: Create Animation View
+    func makeUIView(context: UIViewRepresentableContext<FCLottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        animationView.animation = LottieAnimation.named(nameLottieJsonFile)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play { _ in
+            completion?()
+        }
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        NSLayoutConstraint.activate([
+                                        animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+                                        animationView.widthAnchor.constraint(equalTo: view.widthAnchor)])
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<FCLottieView>) {}
+}
